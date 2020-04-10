500f7c1c40c7b34ed3064d4c6325972db2f028e9
liu lei <1206821643@qq.com>
2020-04-10 21:00:01 +0800

    搜索框

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,16 @@
 <template>
-	<view >
-	首页
+<!-- 每个组件都有一个根标签，给个类名 -->
+	<view class="index">
+		<!-- 头部search搜索框 -->
+		<view class="search">
+			<view class="search-input">
+				<input placeholder="请输入搜索商品" type="text" />
+			</view>
+			<!-- 搜索结果内容 -->
+			<view class="search-content">
+				
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +18,7 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
 		onLoad() {
@@ -21,5 +31,15 @@
 </script>
 
 <style>
-
+	.search .search-input{
+		background-color: #ea4451;
+		padding: 18rpx;
+	}
+	.search .search-input input{
+		background-color: #fff;
+		font-size: 26rpx;
+		color:#eee ;
+		padding-left:15rpx ;
+		height: 60rpx;
+	}
 </style>
