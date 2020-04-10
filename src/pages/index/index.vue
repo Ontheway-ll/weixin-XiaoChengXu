fb37b48b040cca8b4651db4d346e33f867b59a2b
liu lei <1206821643@qq.com>
2020-04-10 17:22:34 +0800

    初始化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,6 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+	<view >
+	首页
 	</view>
 </template>
 
@@ -24,19 +21,5 @@
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
 </style>
