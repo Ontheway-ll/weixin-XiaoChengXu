c1a6ed1b1f5899ac96c0e41db2452a68c3ee4de8
liu lei <1206821643@qq.com>
2020-04-11 23:45:25 +0800

    主页基本布局

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,35 +9,126 @@
       <!-- 搜索结果内容 -->
       <view class="search-content"></view>
     </view>
-	<!-- 轮播图 -->
-  <!-- 255,0,0 红色，255,255,255白色 -->
-  <!-- indicator-dots="true"字符串
+    <!-- 轮播图 -->
+    <!-- 255,0,0 红色，255,255,255白色 -->
+    <!-- indicator-dots="true"字符串
        :indicator-dots="true"变量
-       或者 indicator-dots -->
-	<view class="swiper-box">
-		<swiper :indicator-dots="true" 
-             indicator-color="rgba(255,0,0,.8)"
-            :autoplay="true">
-			<swiper-item>
-				<navigator url="" class="nav">
-					<img src="../../static/uploads/banner1.png" alt="">
-				</navigator>
-			</swiper-item>
+    或者 indicator-dots-->
+    <view class="swiper-box">
+      <swiper :indicator-dots="true" indicator-color="rgba(255,0,0,.8)" :autoplay="true">
+        <swiper-item>
+          <navigator url class="nav">
+            <img src="../../static/uploads/banner1.png" alt />
+          </navigator>
+        </swiper-item>
 
-      	<swiper-item>
-				<navigator url="" class="nav">
-					<img src="../../static/uploads/banner2.png" alt="">
-				</navigator>
-			</swiper-item>
+        <swiper-item>
+          <navigator url class="nav">
+            <img src="../../static/uploads/banner2.png" alt />
+          </navigator>
+        </swiper-item>
 
-      	<swiper-item>
-				<navigator url="" class="nav">
-					<img src="../../static/uploads/banner3.png" alt="">
-				</navigator>
-			</swiper-item>
-			
-		</swiper>
-	</view>
+        <swiper-item>
+          <navigator url class="nav">
+            <img src="../../static/uploads/banner3.png" alt />
+          </navigator>
+        </swiper-item>
+      </swiper>
+    </view>
+    <!-- 导航部分 -->
+    <view class="nav">
+      <navigator url>
+        <img src="../../static/uploads/icon_index_nav_1@2x.png" alt />
+      </navigator>
+      <navigator url>
+        <img src="../../static/uploads/icon_index_nav_2@2x.png" alt />
+      </navigator>
+      <navigator url>
+        <img src="../../static/uploads/icon_index_nav_3@2x.png" alt />
+      </navigator>
+      <navigator url>
+        <img src="../../static/uploads/icon_index_nav_4@2x.png" alt />
+      </navigator>
+    </view>
+    <!-- 内容部分 -->
+    <view class="box">
+      <!-- 每个楼层包含标题和5个图片内容 -->
+      <view class="floor">
+        <!-- 标题 -->
+        <view class="floor-title">
+          <img src="../../static/uploads/pic_floor01_title.png" alt />
+        </view>
+        <!-- 5个图片 -->
+        <view class="item">
+          <navigator url>
+            <img src="../../static/uploads/pic_floor01_1@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor01_2@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor01_3@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor01_4@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor01_5@2x.png" alt />
+          </navigator>
+        </view>
+      </view>
+       <!-- 每个楼层包含标题和5个图片内容 -->
+      <view class="floor">
+        <!-- 标题 -->
+        <view class="floor-title">
+          <img src="../../static/uploads/pic_floor02_title.png" alt />
+        </view>
+        <!-- 5个图片 -->
+        <view class="item">
+          <navigator url>
+            <img src="../../static/uploads/pic_floor02_1@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor02_2@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor02_3@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor02_4@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor02_5@2x.png" alt />
+          </navigator>
+        </view>
+      </view>
+      <!-- 每个楼层包含标题和5个图片内容 -->
+      <view class="floor">
+        <!-- 标题 -->
+        <view class="floor-title">
+          <img src="../../static/uploads/pic_floor01_title.png" alt />
+        </view>
+        <!-- 5个图片 -->
+        <view class="item">
+          <navigator url>
+            <img src="../../static/uploads/pic_floor03_1@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor03_2@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor03_3@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor03_4@2x.png" alt />
+          </navigator>
+          <navigator url>
+            <img src="../../static/uploads/pic_floor03_5@2x.png" alt />
+          </navigator>
+        </view>
+      </view>
+     
+    </view>
   </view>
 </template>
 
@@ -55,29 +146,86 @@ export default {
 .search {
   .search-input {
     background-color: #ea4451;
-	padding: 18rpx;
-	}
+    padding: 18rpx;
     input {
       background-color: #fff;
       font-size: 26rpx;
-      color: #eee;
+      color: black;
       padding-left: 15rpx;
       height: 60rpx;
     }
-  
+  }
 }
-.swiper-box{
+.swiper-box {
   // swiper默认有一个高度是150rpx
-   swiper{
+  swiper {
     height: 340rpx;
-    navigator{
+    navigator {
       width: 750rpx;
-      height:340rpx ;
-      img{
-       width:100%;
-       height:100%;
+      height: 340rpx;
+      img {
+        width: 100%;
+        height: 100%;
       }
     }
-  } 
+  }
+}
+.index .nav {
+  padding: 30rpx 0;
+  display: flex;
+  navigator {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+}
+.box {
+  .floor {
+    .floor-title {
+      background-color: #f4f4f4;
+      padding-top: 30rpx;
+      img {
+        width: 750rpx;
+        height: 60rpx;
+      }
+    }
+    .item {
+      padding: 20rpx 20rpx;
+      overflow: hidden;
+      navigator {
+        float: left;
+        width: 193rpx;
+        height: 188rpx;
+        margin-left: 10rpx;
+        margin-bottom: 10rpx;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+      navigator:nth-child(1) {
+        height: 386rpx;
+        margin-left: 0;
+      }
+      navigator:nth-child(2),
+      navigator:nth-child(5){
+          width: 273rpx;
+          
+      }
+    }
+  }
+  // 处理第一个floor里第2和5
+  .floor:nth-child(1){
+    .item{
+      navigator{
+      width: 223rpx;
+    }
+    }
+  }
 }
 </style>
